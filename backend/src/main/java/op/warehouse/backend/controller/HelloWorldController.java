fa0adb3346c5ed7b4a739a5e14a73b0beeaf5c96
JackyBlackson <Jacky_Blackson@outlook.com>
2024-03-02 17:41:18 +0800

    init: inited backend
    Inited backend, and added a hello-world example controller.

---
+++ backend/src/main/java/op/warehouse/backend/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package op.warehouse.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, OP";
+    }
+}
---
+++ backend/src/test/java/op/warehouse/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package op.warehouse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
