81f0b7875556b5a469faa41e43258c544c7d6027
SHIDi233 <1985136419@qq.com>
2024-03-12 13:45:59 +0800

    upload and regist test

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,9 +5,11 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from 'axios'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+app.provide('$axios',axios)
 app.mount('#app')
