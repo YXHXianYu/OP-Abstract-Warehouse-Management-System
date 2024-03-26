041a2036a7174d7470563985dfc88e1ea3465d25
SHIDi233 <1985136419@qq.com>
2024-03-26 12:38:23 +0800

    change some css

--- frontend/src/components/Cargo.vue
+++ frontend/src/components/Cargo.vue
@@ -1,36 +1,49 @@
 <template>
-    <el-button type="success" size="small" @click="dialogFormVisible=true;">添加根节点</el-button>
-    <el-button type="success" size="small" @click="dialogFormVisible_2=true;">添加子节点</el-button>
-    <el-tree
-        style="max-width: 600px"
-        :data="tree"
-        @node-click="handleNodeClick"
-    />
-    <el-dialog v-model="dialogFormVisible" title="新建根节点  " width="500">
+    <el-card style="width: 100%">
+        <el-button style="margin-bottom: 20px;" type="success" size="small" @click="dialogFormVisible=true;">添加根节点</el-button>
+        <el-tree
+            style="max-width: 100%"
+            :data="tree"
+            @node-click="handleNodeClick"
+            :highlight-current="true"
+            v-loading="loading"
+        >
+            <template #default="{ node, data }">
+                <span class="custom-tree-node">
+                    <span>{{ node.label }}</span>
+                    <span>
+                        <el-button type="primary" size="small" :icon="Plus" @click="dialogFormVisible_2=true;" circle></el-button>
+                        <el-button type="danger" size="small" :icon="Minus" @click="" circle></el-button>
+                    </span>
+                </span>
+            </template>
+        </el-tree>
+    </el-card>
+    <el-dialog v-model="dialogFormVisible" title="新建根节点" width="500">
         <el-form :model="form" label-width="auto" style="max-width: 600px">
-            <el-form-item label="name:">
+            <el-form-item label="根节点名称:">
                 <el-input v-model="form.name" />
             </el-form-item>
-            <el-form-item label="description:">
+            <el-form-item label="根节点描述:">
                 <el-input v-model="form.description" />
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" style="float:right" @click="commit">Create</el-button>
-            </el-form-item>
         </el-form>
+        <div style="text-align: center;">
+            <el-button type="primary" v-loading="loading_2" @click="commit">创建</el-button>
+        </div>
     </el-dialog>
-    <el-dialog v-model="dialogFormVisible_2" title="新建子节点  " width="500">
+    <el-dialog v-model="dialogFormVisible_2" title="新建子节点" width="500">
         <el-form :model="form" label-width="auto" style="max-width: 600px">
-            <el-form-item label="name:">
+            <el-form-item label="子节点名称:">
                 <el-input v-model="form.name" />
             </el-form-item>
-            <el-form-item label="description:">
+            <el-form-item label="子节点描述:">
                 <el-input v-model="form.description" />
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" style="float:right" @click="commit_2">Create</el-button>
-            </el-form-item>
         </el-form>
+        <div style="text-align: center;">
+            <el-button type="primary" v-loading="loading_2" @click="commit_2">创建</el-button>
+        </div>
     </el-dialog>
 </template>
 
@@ -41,6 +54,12 @@
     axios.defaults.baseURL = '/api' 
     import { ElNotification } from 'element-plus'
     import { reactive } from 'vue'
+    import {
+        Plus,Minus
+    } from '@element-plus/icons-vue'
+
+    const loading=ref(true);
+    const loading_2=ref(false);
 
     interface Tree {
         label: string
@@ -76,12 +95,14 @@
         return res;
     };
 
-    onMounted(() => {
+    const refresh=()=>{
+        loading.value=true;
         axios.get('/cargo-types/trees')
         .then(function (res) {
             if(res.data.message=='ok'){
                 console.log(parse(res.data.data));
                 tree.value = parse(res.data.data);
+                loading.value=false;
             }
             else{
                 ElNotification({
@@ -98,6 +119,12 @@
                 type: 'error',
             })
         });
+    };
+
+    
+
+    onMounted(() => {
+        refresh();
     });
 
     const cargoID = ref("-1");
@@ -117,7 +144,10 @@
                     title: 'Success',
                     message: '创建成功',
                     type: 'success',
-                })  
+                });
+                dialogFormVisible.value=false;
+                refresh();
+                loading_2.value=false;
             }
             else{
                 ElNotification({
@@ -145,14 +175,18 @@
                     title: 'Success',
                     message: '创建成功',
                     type: 'success',
-                })  
+                });
+                loading_2.value=false;
+                dialogFormVisible_2.value=false;
+                refresh();
             }
             else{
                 ElNotification({
                     title: '创建错误',
                     message: res.data.data,
                     type: 'error',
-                })
+                });
+                loading_2.value=false;
             }
         })
         .catch(function (error) {
@@ -160,15 +194,33 @@
                 title: '网络错误',
                 message: error,
                 type: 'error',
-            })
+            });
+            loading_2.value=false;
         });
     };
 
     const commit=()=>{
+        loading_2.value=true;
         addRoot();
+        form.name='';
+        form.description='';
     };
     const commit_2=()=>{
+        loading_2.value=true;
         if(cargoID.value=='-1'){alert("请选中后添加节点");return};
         addChild();
+        form.name='';
+        form.description='';
     };
 </script>
+
+<style>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
+}
+</style>
