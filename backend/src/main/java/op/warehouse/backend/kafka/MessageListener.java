52a1b29cf4cc88b434ca16a510d9386541139c21
Apple_Grey <2982437139@qq.com>
2024-05-28 10:39:50 +0800

    feat: perfect the messaging

--- backend/src/main/java/op/warehouse/backend/kafka/MessageListener.java
+++ backend/src/main/java/op/warehouse/backend/kafka/MessageListener.java
@@ -1,6 +1,9 @@
 package op.warehouse.backend.kafka;
 
 import com.alibaba.fastjson.JSON;
+import op.warehouse.backend.entity.CargoItem;
+import op.warehouse.backend.service.CargoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,19 +12,23 @@ import java.util.Map;
 @Component
 public class MessageListener {
 
+    @Autowired
+    CargoItemService cargoItemService;
+
     @KafkaListener(topics = "accomplish", groupId = "testGroup")
     public void listenAccomplish(String message) {
         System.out.println("Received Accomplish message: " + message);
         Map<String, Object> m = JSON.parseObject(message, Map.class);
-        int cargoID = (int) m.get("cargoID");
-
+        Long cargoID = (Long) m.get("cargoID");
+        cargoItemService.changeState(cargoID, CargoItem.StateEnum.READY);
     }
 
     @KafkaListener(topics = "start", groupId = "testGroup")
     public void listenStart(String message) {
         System.out.println("Received Start message: " + message);
         Map<String, Object> m = JSON.parseObject(message, Map.class);
-        int cargoID = (int) m.get("cargoID");
+        Long cargoID = (Long) m.get("cargoID");
+        cargoItemService.changeState(cargoID, CargoItem.StateEnum.ON_BOARDING);
     }
 
 }
